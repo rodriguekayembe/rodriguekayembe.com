2a225a7546ac4d03369b4a782875e905a95be57c
Rodrigue Kayembe <rodriguekayembe@gmail.com>
2023-11-01 21:45:50 +0000

    Revert "netlify"
    This reverts commit 7d93c13e0af02f8f4b87f0805bd452624bdba730.

--- presta.config.ts
+++ presta.config.ts
@@ -2,5 +2,5 @@ import sourceFs from '@presta/source-filesystem'
 import netlify from '@presta/adapter-netlify'
 
 
-export const files = ['pages/*.ts'];
+export const files = ["pages/*.ts"];
 export const plugins = [sourceFs(), netlify()]
