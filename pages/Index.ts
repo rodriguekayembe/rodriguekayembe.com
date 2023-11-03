f8c2f5f082d290b23aa3c5b7099114af8401db26
Rodrigue Kayembe <rodriguekayembe@gmail.com>
2023-11-03 17:05:42 +0000

    Styling and content
    Add index and styling.

--- pages/Index.ts
+++ pages/Index.ts
@@ -10,7 +10,34 @@ export async function handler(event) {
   return html({
     head: createHeadObject(),
     body: `
-    <div class='p10'>
+    <div class = "content">
+      <div class = "title"> 
+      Rodrigue Kayembe
+      </div>
+      <div class = "section bio">
+      I'm a software engineer currently on mini retirement. Previously worked at <a href="https://www.alfasystems.com/" target="_blank">Alfa</a> and in a previous life studied law. <br><br>
+      I was born in Tshikapa, grew up in Dublin, and I am a part-time Londoner. <br><br>
+
+      I'm interested in travelling, reading, basketball, lifting, storytelling, entrepreneurship, technology & the internet.
+
+      </div>
+      
+      <div class="section projects" id = "projects">
+        <b> Projects </b> 
+      <div>    
+      
+      <div class="section writing" id = "writing" >
+        <b> Writing<b> 
+      <div>
+      
+      <div  class="section misc" id = "misc" >
+        <b>Contact/Miscellaneous</b>
+        <ul class="misc-list">
+        <li><a href=""https://github.com/rodriguekayembe" target="_blank">Github</a>
+        <li><a href="https://bsky.app/profile/rodriguekayembe.com" target="_blank">Bluesky</a>
+        <li><a href="/scratchpad" target="_blank">Scratchpad</a>
+        <li><a target="_blank">Email: contact (at) rodriguekayembe (dot) com</a>
+      </div>
     </div>
   `,
   })
