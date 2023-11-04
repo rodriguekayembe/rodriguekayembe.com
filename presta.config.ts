88c2a9d648f4813b06cec3e74fdb0006e5675676
Rodrigue Kayembe <rodriguekayembe@gmail.com>
2023-11-04 12:23:22 +0000

    Update presta.config.ts
    forced to use createConfig as Presta updated

--- presta.config.ts
+++ presta.config.ts
@@ -1,6 +1,8 @@
-import sourceFs from '@presta/source-filesystem'
+import { createConfig } from 'presta'
 import netlify from '@presta/adapter-netlify'
 
 
-export const files = ["pages/*.ts"];
-export const plugins = [sourceFs(), netlify()]
+export default createConfig({
+  files: ['pages/*'],
+  plugins: [netlify()],
+})
