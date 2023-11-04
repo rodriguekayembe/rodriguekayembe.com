28c06dc0b4c0b09a8754d081296496f68e8d2f98
Rodrigue Kayembe <rodriguekayembe@gmail.com>
2023-11-04 13:47:19 +0000

    Dependency fix

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,7 @@
-import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import adapter from '@sveltejs/adapter-netlify';
+
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -12,6 +14,7 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter()
+
 	}
 };
 
