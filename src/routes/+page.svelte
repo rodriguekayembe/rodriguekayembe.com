626296c923f99b0b1bf081865d7a20ffaeb4ade7
Rodrigue Kayembe <rodriguekayembe@gmail.com>
2023-11-06 01:45:00 +0000

    Add font
    Import

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,35 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<body>
+	<div class="content">
+		<div class="title">Rodrigue Kayembe</div>
+		<div class="section bio">
+			I'm a software engineer currently on mini retirement. Previously worked at <a
+				href="https://www.alfasystems.com/"
+				target="_blank">Alfa</a
+			>
+			and in a previous life studied law. <br /><br />
+			I was born in Tshikapa, grew up in Dublin, I am a part-time Londoner and part-time wanderer.
+			<br /><br />
+
+			I'm interested in travelling, reading, basketball, lifting, storytelling, entrepreneurship,
+			technology & the internet.
+		</div>
+
+		<div class="section projects" id="projects">
+			<b> Projects </b>
+		</div>
+
+		<div class="section writing" id="writing">
+			<b> Writing<b /></b>
+		</div>
+
+		<div class="section misc" id="misc">
+			<b>Contact/Miscellaneous</b>
+			<ul class="misc-list">
+				<li><a href="https://github.com/rodriguekayembe" target="_blank">Github</a></li>
+				<li><a href="https://bsky.app/profile/rodriguekayembe.com" target="_blank">Bluesky</a></li>
+				<li><a href="/scratchpad" target="_blank">Scratchpad</a></li>
+				<li>Email: contact (at) rodriguekayembe (dot) com</li>
+			</ul>
+		</div>
+	</div>
+</body>
