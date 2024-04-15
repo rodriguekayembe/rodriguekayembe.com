2907244a4f0e45e1eee54209ec270a32f4e1951f
rodrigue <rodriguekayembe@gmail.com>
2024-04-15 20:19:55 +0100

    Update landing.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,45 +1,34 @@
-<body>
+<div>
 	<div class="content">
 		<div class="title">Rodrigue Kayembe</div>
 		<div class="section bio">
 			I'm a software engineer, previously worked at <a
-				href="https://www.alfasystems.com/"
-				target="_blank">Alfa</a
-			>
-			and in a previous life studied law. <br /><br />
+			href="https://www.alfasystems.com/"
+			target="_blank">Alfa</a
+		><br /><br />
 			I was born in Tshikapa, and grew up in Dublin, I am a part-time Londoner and part-time wanderer.
-			<br /><br />
-
-			I'm interested in travelling, reading, basketball, lifting, storytelling, entrepreneurship,
-			technology & the internet.
 		</div>
-
 		<div class="section projects" id="projects">
 			<b> Projects </b>
-			 <ul class= "misc-list">
-				<li><a href="https://forgottentapes.com" target="_blank">Forgotten Tapes - Streaming library transfer tool (WIP)</a></li>
-			 </ul>
+			<ul class="misc-list">
+				<li><a href="https://forgottentapes.com" target="_blank"> Forgotten Tapes - Transfer library across Apple Music/Spotify </a></li>
+			</ul>
+		</div>
+		<div class="section writing" id="writing">
+			<b><a href="/">Writing</a></b>
 		</div>
 
-		<div class="section writing" id="writing">
-			<b><a href="/">Writing</a> <b/>
-		</div>
-		
 		<div class="section reading" id="reading">
 			<b> Bookshelf </b>
 			<br>
-			Reading: Madeline Miller ~ Circe & Mark Tegmark ~ Life 3.0  
-			
+			Reading: Madeline Miller ~ Circe
 		</div>
-
 		<div class="section misc" id="misc">
 			<b>Contact/Miscellaneous</b>
 			<ul class="misc-list">
 				<li><a href="https://github.com/rodriguekayembe" target="_blank">Github</a></li>
-				<!-- <li><a href="https://bsky.app/profile/rodriguekayembe.com" target="_blank">Bluesky</a></li> -->
-				<!-- <li><a href="/scratchpad">Scratchpad</a></li> -->
 				<li>Email: contact (at) rodriguekayembe (dot) com</li>
 			</ul>
 		</div>
 	</div>
-</body>
+</div>
