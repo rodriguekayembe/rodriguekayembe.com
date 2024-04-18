dd846b4175b4af9233a2d655d64919cb96f7e72c
rodrigue <rodriguekayembe@gmail.com>
2024-04-18 22:18:20 +0100

    Update Feed component.

--- src/components/Feed.svelte
+++ src/components/Feed.svelte
@@ -1,37 +1,41 @@
 <script lang="ts">
-	import { type Item, Omnivore } from '@omnivore-app/api';
+	import { isOmnivoreError, type Item, Omnivore } from '@omnivore-app/api';
 	import { onMount } from 'svelte';
 
 	let items: Item[] = [];
 
 	onMount(async () => {
-		console.log("im here")
-		const omnivore : Omnivore = new Omnivore({
-			apiKey: '598ded3e-4021-4bdf-ab¬fgb0-f0268274fe0c',
-			baseUrl: 'https://api-prod.omnivore.app',
+		const omnivore: Omnivore = new Omnivore({
+			apiKey: '',
 			timeoutMs: 100000 // 10 seconds
 		});
 
-		const response = await omnivore.items.search({
-			after: 0,
-			first: 10,
-			format: "html",
-			includeContent: true,
-			query: ``
-		})
+		try {
+			const response = await omnivore.items.search({
+				after: 0,
+				first: 10,
+				format: 'highlightedMarkdown',
+				includeContent: true,
+				query: ``
+			});
+			items = response.edges.map((e) => e.node);
 
-		items = response.edges.map((e) => e.node)
+		} catch (e) {
+			if (isOmnivoreError(e)) {
+				console.log(e);
+			}
+
+		}
+
 	});
-
-
 </script>
 
 
-{#each items?.filter(item => item.highlights && item.highlights.length > 0) as item}	<p>
-		<a href={item.url}>
-			{item.title}
-		</a>
-	</p>
+{#each items?.filter(item => item.highlights && item.highlights.length > 0) as item}  <p>
+	<a href={item.url}>
+		{item.title}
+	</a>
+</p>
 {/each}
 
 
